3f56973e374f4299edf0a8d8718ea126d6f562e8
Layne <cs1181162984@163.com>
2023-05-09 12:14:37 +0800

    second commit

--- src/main/java/com/boot/git/GitTest.java
+++ src/main/java/com/boot/git/GitTest.java
@@ -9,6 +9,7 @@ package com.boot.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
