7814816d5edfacf02ae3e16c21f40d001de59e8b
Layne <cs1181162984@163.com>
2023-05-09 11:48:01 +0800

    first commit

---
+++ src/main/java/com/boot/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.boot.git;
+
+/**
+ * @Description
+ * @Classname:GitTest
+ * @Date：2023/5/9 11:41
+ * @Created：csir
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
